bc0a3b1fa6d0b42486d7a1f3b293eda4e7c6fa82
virada-k <virada.kumpiranont@gmail.com>
2026-02-08 18:17:10 +0700

    Update reorder.py

--- 03-machine-learning/repeat-order-python/reorder.py
+++ 03-machine-learning/repeat-order-python/reorder.py
@@ -1,6 +1,7 @@
-# 📢 Data Preparation
+# 📢 Data Preparation & Machine Learning
+# Project: Instacart Repeat Order Prediction
 
-## Library Imports
+# --- Library Imports ---
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
@@ -8,8 +9,13 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import classification_report, roc_auc_score
 import gdown
 
-## Data Loading
+# --- Data Loading ---
 # Since the dataset is large, it is hosted on Google Drive.
 # You can replace the path below with your local file path.
 # df = pd.read_csv('instacart_data.csv')
 
+## Store 'orders' in a Dataframe.
+orders = pd.read_csv('orders.csv',
+                     usecols=['order_id', 'user_id', 'eval_set', 'order_number', 'days_since_prior_order'])
+
+print(orders.head())
