35b9c1b42eaaf4f03147d377e43b34a7cc73afef
virada-k <virada.kumpiranont@gmail.com>
2026-02-08 17:24:49 +0700

    Update reorder.py

--- 03-machine-learning/repeat-order-python/reorder.py
+++ 03-machine-learning/repeat-order-python/reorder.py
@@ -1,6 +1,15 @@
+# 📢 Data Preparation
+
+## Library Imports
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import classification_report, roc_auc_score
 import gdown
+
+## Data Loading
+# Since the dataset is large, it is hosted on Google Drive.
+# You can replace the path below with your local file path.
+# df = pd.read_csv('instacart_data.csv')
+
