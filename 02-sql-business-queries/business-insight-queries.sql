95c9716c59ee289829da10e6e86aa75e8537031c
virada-k <virada.kumpiranont@gmail.com>
2026-02-20 17:16:37 +0700

    Update business-insight-queries.sql

--- 02-sql-business-queries/business-insight-queries.sql
+++ 02-sql-business-queries/business-insight-queries.sql
@@ -1,5 +1,5 @@
 -- 📌 GOLDEN HOUR ANALYSIS
--- Analyzing "PEAK Ordering Time" to arrange staff for packing and delivering orders.
+-- Analyze "PEAK Ordering Time" to arrange staff for packing and delivering orders.
 
 SELECT
     order_hour_of_day AS order_hour,
@@ -9,12 +9,12 @@ GROUP BY order_hour
 ORDER BY count_order_hour DESC
 LIMIT 10;
 
--- 📢 The data shows that the peak time for customers purchase is between 9 am and 5 pm.
+-- 📢 Note: The data shows that the peak shopping time for customers is between 9 a.m. and 5 p.m.
 
 
 
 -- 📌 GOLDEN DAY ANALYSIS
--- Analyze the days with the highest customers order volume to prepare the stock.
+-- Analyze the days with the highest customer orders volume to prepare the stock.
 
 SELECT
     CASE
@@ -31,7 +31,30 @@ FROM orders
 GROUP BY order_dow
 ORDER BY count_order_day DESC;
 
--- 📢 The data shows that the three days with the highest number of product orders are Sunday to Tuesday.
+-- 📢 Note: The data shows that the three days with the highest number of product orders are Sunday to Tuesday.
+
+
+
+-- 📌 OFF-PEAK STRATEGY ANALYSIS
+-- Analyze the days and times with the fewest customer orders to organize a flash sale promotion (special discount).
+
+SELECT
+    CASE
+      WHEN order_dow = 0 THEN 'Sunday'
+      WHEN order_dow = 1 THEN 'Monday'
+      WHEN order_dow = 2 THEN 'Tuesday'
+      WHEN order_dow = 3 THEN 'Wednesday'
+      WHEN order_dow = 4 THEN 'Thursday'
+      WHEN order_dow = 5 THEN 'Friday'
+      ELSE 'Saturday'
+    END AS day_of_week,
+    order_hour_of_day AS order_hour,
+    COUNT(*) AS count_order
+FROM orders
+GROUP BY order_dow, order_hour_of_day
+ORDER BY count_order
+
+-- 📢 Note: The data shows that 
 
 
 
@@ -53,5 +76,5 @@ HAVING total_reorders > 600 -- The number 600 comes from the top 10% of best-sel
 ORDER BY ROUND(AVG(o.days_since_prior_order)) DESC
 LIMIT 10;
 
--- 📢 The data shows that most products follow a typical purchase cycle.
+-- 📢 Note: The data shows that most products follow a typical purchase cycle.
 -- Bulk products, like laundry detergents, are often bought monthly (on average, twice a month).
